d0fc22ea31afdf379569b9c056d68d9954e0e209
Harshal Mahajan <hm2672746@gmail.com>
2026-07-06 01:11:26 +0530

    updated

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
-const app = express();
+const ejsMate = require("ejs-mate");
 const Listing = require("./models/lists");
 
+const app = express();
 const port = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
 async function main() {
@@ -28,55 +30,32 @@ main()
 
 app.get("/", async (req, res) => {
     let lists = await Listing.find();
-    res.render("home.ejs", {lists});
+    res.render("./listings/home.ejs", {lists});
 })
 
 app.get("/listing/new", (req, res)=>{
-    res.render("form.ejs");
+    res.render("./listings/form.ejs");
 })
 app.post("/listing", async (req, res)=>{
-    let {title,description, image, price, location, country} = req.body;
-    console.log(req.body);  
-    let ans = await Listing.insertOne({
-        title:title,
-        description:description,
-        image:{
-            filename:"listingFile",
-            url:image
-        },
-        price:price,
-        location:location,
-        country:country
-    });
-    console.log(ans);
+    let listing = req.body.listing;  
+    let ans = await Listing.insertOne(listing);
     res.redirect("/");  
     
 })
 
-app.get("/listing/update/:id", async(req, res)=>{
+app.get("/listing/edit/:id", async(req, res)=>{
     let {id} = req.params;
     let listing = await Listing.find({_id:id});
     listing = listing[0];
-    console.log(listing);
-    res.render("update.ejs", {listing});
+    res.render("./listings/update.ejs", {listing});
 })
 app.patch("/:id", async(req, res)=>{
     let {id} = req.params;
-    let{title, description, image, price, location, country} = req.body;
-    console.log(req.body);
-    let ans = await Listing.updateOne({_id:id}, {
-        title:title,
-        description:description,
-        image:{
-            filename:"listingfile",
-            url:image
-        },
-        price:price,
-        location:location,
-        country:country
-    })
-    console.log(ans);
+    let listing = req.body.listing;
+    console.log(listing);
+    await Listing.updateOne({_id:id}, listing);
     res.redirect("/");
+    
 })
 
 app.delete("/:id", async (req, res)=>{
@@ -89,8 +68,7 @@ app.get("/listing/:id", async (req, res)=>{
     let {id} = req.params;
     let listing = await Listing.find({_id:id});
     listing = listing[0];
-    console.log(listing);
-    res.render("show.ejs", {listing});
+    res.render("./listings/show.ejs", {listing});
 })
 
 
