a8089ad8cd8f66a75c1c0b089e2902d175d723a2
Harshal Mahajan <hm2672746@gmail.com>
2026-07-16 02:39:15 +0530

    added server side validation

--- index.js
+++ index.js
@@ -4,79 +4,103 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const Listing = require("./models/lists");
+const ExpressError = require("./utils/ExpressError");
+const wrapAsync = require("./utils/wrapAsync");
 
 const app = express();
 const port = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
+
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL)
+
 }
+
 main()
     .then((res) => {
         console.log("Database is connected");
     })
     .catch((err) => {
-        console.log(err);
-    })
+        throw new ExpressError(500, "Database cannot be connect");
+    });
 
-app.get("/", async (req, res) => {
+app.get("/", wrapAsync(async (req, res) => {
     let lists = await Listing.find();
+    if (!lists) {
+        throw new ExpressError(500, "Cannot load Listings");
+    }
     res.render("./layouts/listings/home.ejs", { lists });
-})
+}));
 
 app.get("/listing/new", (req, res) => {
     res.render("./layouts/listings/form.ejs");
-})
-app.post("/listing", async (req, res, next) => {
-    try {
-        let listing = req.body.listing;
-        let ans = await Listing.insertOne(listing);
-        res.redirect("/");
-    } catch (err) {
-        next(err);
-    }
-})
+});
 
-app.get("/listing/edit/:id", async (req, res) => {
+app.post("/listing", wrapAsync(async (req, res, next) => {
+    let listing = req.body.listing;
+    if (!listing) {
+        next(new ExpressError(400, "Listing cannot be added!"));
+    }
+    const newListing = new Listing(listing);
+    await newListing.save();
+    res.redirect("/");
+}));
+
+app.get("/listing/edit/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    let listing = await Listing.find({ _id: id });
-    listing = listing[0];
+    let listing = await Listing.findById(id);
+    if (!listing) {
+        throw new ExpressError(403, "Listing not found!");
+    }
     res.render("./layouts/listings/update.ejs", { listing });
-})
-app.patch("/:id", async (req, res) => {
+}));
+
+app.patch("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = req.body.listing;
+    if (!listing) {
+        throw new ExpressError(403, "Listing not found!");
+    }
     await Listing.findOneAndUpdate({ _id: id }, listing);
     res.redirect("/");
-})
+}));
 
-app.delete("/:id", async (req, res) => {
+app.delete("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.deleteOne({ _id: id });
     res.redirect("/");
-})
+}));
 
-app.get("/listing/:id", async (req, res) => {
+app.get("/listing/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    let listing = await Listing.find({ _id: id });
-    listing = listing[0];
+    let listing = await Listing.findById(id);
+    if (!listing) {
+        throw new ExpressError(403, "Listing not found!");
+    }
     res.render("./layouts/listings/show.ejs", { listing });
-})
+}));
+
+app.all("/{*splat}", (req, res, next) => {
+    next(new ExpressError(404, "Page not found!"));
+});
 
 app.use((err, req, res, next) => {
     let { status = 400, message = "Something is going on wrong" } = err;
-    res.status(status).send(message);
-})
+    // res.status(status).send(message);
+    res.status(status).render("error.ejs",{err});
+});
 
 app.listen(port, () => {
     console.log("server is running");
-})
+});
