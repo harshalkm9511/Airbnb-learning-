7c332f77ea975affa2152328ef604206290b2ed3
Harshal Mahajan <hm2672746@gmail.com>
2026-08-06 02:22:38 +0530

    added Authentication feature

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 const ExpressError = require("./utils/ExpressError");
 const listingRouter = require("./router/listing.js");
 const reviewRouter = require("./router/reviews.js");
+const User = require("./models/user.js");
 
 const app = express();
 const port = 8080;
@@ -37,7 +40,6 @@ async function main() {
 
 main();
 
-
 app.get("/", (req, res) => {
     res.redirect("/listing");
 })
@@ -58,6 +60,21 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/demoUser", async(req, res)=>{
+    let fakeUser = new User({
+        email:"harshal101@gmail.com",
+        username:"harshal101"
+    });
+
+    let registerUser = await User.register(fakeUser, "harshal@123");
+    res.send(registerUser);
 })
 
 app.use("/listing", listingRouter);
